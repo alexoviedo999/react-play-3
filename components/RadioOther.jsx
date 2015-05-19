486fbbc7f4b452209d9353057c05ca5c7ba3942f
alexoviedo <alexoviedo999@gmail.com>
2015-05-19 12:26:52 -0400

    radio toggling text input.

--- components/RadioOther.jsx
+++ components/RadioOther.jsx
@@ -1,16 +1,40 @@
 var RadioOtherOption = React.createClass({
+	getInitialState: function () {
+	    return {
+	        checked: false 
+	    };
+	},
+	onChange: function(event){
+		var input = event.target;
+
+		this.setState({
+			checked: input.checked
+		});
+	},
+	componentDidUpdate: function (prevProps, prevState) {
+	  var input = this.refs.input.getDOMNode();
+
+	  if (prevState.checked !== input.checked) {
+			this.setState({
+				checked: input.checked
+			});
+	  }
+	},
 	render: function(){
 		return (
 			<p className="form-group radio">
 				<label>
-					<input type="radio" name="referrer" value="other"/>
+					<input ref="input" onChange={this.onChange} type="radio" name="referrer" value="other"/>
 					Other
 				</label>
-				<label className="form-inline">
-					Please specify:
-					<input type="text" name="referrer_other"/>
-				</label>
+
+				{this.state.checked && (
+					<label className="form-inline">
+						Please specify:
+						<input type="text" name="referrer_other"/>
+					</label>
+				)}
 			</p>
-		)
+		);
 	}
-})
+});
