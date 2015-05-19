237118a0852f777241f6c0490b1d6c8d5d518531
alexoviedo <alexoviedo999@gmail.com>
2015-05-19 14:55:17 -0400

    added build files to app.

---
+++ build/Poll.js
@@ -0,0 +1,25 @@
+var Poll = React.createClass({displayName: "Poll",
+	render: function () {
+		var radioOptions = [
+			{ value: 'newspaper', label: 'Newspaper'},
+			{ value: 'radio', label: 'Radio'},
+			{ value: 'tv', label: 'Television' },
+			{ value: 'search', label: 'Search Engine' },
+			{ value: 'social', label: 'Social Media'}
+		];
+
+		return (
+				React.createElement("div", {className: "container"}, 
+					React.createElement("h1", null, "Poll"), 
+					
+					React.createElement("form", null, 
+						React.createElement("p", {className: "h3"}, "How did you hear about us?"), 
+
+						React.createElement(RadioOptionGroup, {name: "referrer", other: true, options: radioOptions}), 
+
+						React.createElement("p", null, React.createElement("input", {type: "submit"}))
+					)
+				)
+			)
+	}
+})
